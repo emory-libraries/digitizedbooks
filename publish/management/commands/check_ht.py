6fc67d97a9a2c183faa8389678e60bfc155de6b8
Jay Varner <jayvarner@gmail.com>
2015-05-22 16:00:26 -0400

    Added some more comments to manage comand.

--- publish/management/commands/check_ht.py
+++ publish/management/commands/check_ht.py
@@ -1,6 +1,6 @@
 """
 Manage command to check if the volume is live in HatiTrust. If it is
-we will updete the KDip with to make the attribute ``accepted_by_ht`
+we will updete the KDip making the attribute `accepted_by_ht`
 `True` and then update the PID with the ht_url
 """
 from django.core.management.base import BaseCommand
@@ -18,7 +18,7 @@ class Command(BaseCommand):
     def handle(self, **options):
         """
         HT returns a `200` for volumes that are live.
-        If not found, `500` is returned
+        If not found, `404` is returned.
         """
         kdips = KDip.objects.filter(accepted_by_ht=False)
         ht_stub = getattr(settings, 'HT_STUB', None)
@@ -28,13 +28,18 @@ class Command(BaseCommand):
         for kdip in kdips:
             ht_url = '%s%s' % (ht_stub, kdip.kdip_id)
             req = requests.get(ht_url)
+            # If we get 200, we call it good and updte the KDip.
             if req.status_code == 200:
                 kdip.accepted_by_ht = True
                 kdip.save()
 
+                # Update the PID in pidman the the HathiTrust URL.
                 client.update_target( \
                     type="ark", noid=kdip.pid, target_uri=ht_url)
 
+                # Try to remove the zip file that had been sent the HT.
+                # We except the `OSError` because the file might have
+                # already been cleaned out.
                 kdip_dir = getattr(settings, 'KDIP_DIR', None)
                 try:
                     remove('%sHT/%s.zip' % (kdip_dir, kdip.kdip_id))
