d6eae676ca94845558a593c7ddf6b73de3656d97
Jay Varner <jayvarner@gmail.com>
2015-06-10 14:25:50 -0400

    Fixed slight issue with check_al command.

--- publish/management/commands/check_al.py
+++ publish/management/commands/check_al.py
@@ -4,7 +4,7 @@ from publish.models import KDip, load_local_bib_record
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
-        kdips = KDip.objects.filter(accepted_by_ht=False).exclude(al_ht=True)
+        kdips = KDip.objects.filter(accepted_by_ht=True).exclude(al_ht=True)
 
         for kdip in kdips:
             marc_rec = load_local_bib_record(kdip.kdip_id)
@@ -12,6 +12,6 @@ class Command(NoArgsCommand):
             link = 'http://pid.emory.edu/ark:/25593/%s/HT' % kdip.pid
 
             if link.decode('utf-8').lower() in marc_rec.serialize().decode('utf-8').lower():
+                print 'true'
                 kdip.al_ht = True
                 kdip.save()
-                
