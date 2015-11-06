e9348c3dcd3143b0e31b54822d96202584097d06
Jay Varner <jayvarner@gmail.com>
2015-11-06 12:48:42 -0500

    Update to celery file for new code organization.

--- digitizedbooks/celery.py
+++ digitizedbooks/celery.py
@@ -9,7 +9,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digitizedbooks.settings')
 
 from django.conf import settings
 
-app = Celery('digitizedbooks', include=['publish'])
+app = Celery('digitizedbooks', include=['digitizedbooks.apps.publish'])
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
